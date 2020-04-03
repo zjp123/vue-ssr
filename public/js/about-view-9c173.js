a308f0fdcb3ed40a221363a32e2d8b910a62a6e7
张灵坦 <zhanglingtan@zlt.lan>
2020-04-03 13:36:34 +0800

    补充说明一些

---
+++ public/js/about-view-9c173.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{51:function(t,n,a){"use strict";a.r(n);var s={data:()=>({about:"阿萨德开始看看书看的"})},e=a(1),i=Object(e.a)(s,(function(){var t=this.$createElement;return(this._self._c||t)("div",[this._v("\n  这是about页面"+this._s(this.about)+"\n")])}),[],!1,null,"66a1b708",null);n.default=i.exports}}]);
--- redeme.md
+++ redeme.md
@@ -1,3 +1,11 @@
 # vue vue-touter vuex ssr koa
 # npm build  打包正式环境  npm start 正式环境启动
 # 开发时  npm run dev:client  npm run dev:server
+```
+发布：
+登陆服务器，Git clone 项目，npm install 先看服务器有没有node环境 npm build ,
+然后 pm2 启动node ,即可  在配合nginx 做转发。。。
+
+或者把打包好的文件直接上传服务器
+
+```
